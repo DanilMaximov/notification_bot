a4bb086afe6103d4a8861e55778f66665b040945
Nerbyk <danil.maximov@flatstack.com>
2023-01-13 13:42:41 +0100

    setup local tg bot server (ngrok + rack)

--- src/client/src/bot_client.rb
+++ src/client/src/bot_client.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
+require "json"
+
 module BotClient
   class Application
-    def self.call
-      puts "Hello, world!"
+    TELEGRAM_BOT_TOKEN = ENV["TELEGRAM_BOT_TOKEN"].freeze
+
+    def self.handler(event:, context:)
+      puts ENV["TELEGRAM_BOT_TOKEN"]
+      body = JSON.parse(event[:body], symbolize_names: true)
+
+      return {}.to_json unless body[:message][:chat][:type] == "private"
+
+      handler = ::CommandHandlerFunction
+
+      handler.process(:start, body.slice(:message))
     end
   end
 end
--- src/client/test/bot_client_test.rb
+++ src/client/test/bot_client_test.rb
@@ -1,8 +1,10 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 describe BotClient do
   it "application must be defined" do
     assert_kind_of Class, BotClient::Application
-    assert_kind_of Method, BotClient::Application.method(:call)
+    assert_kind_of Method, BotClient::Application.method(:handler)
   end
 end
