c15d52e0bc4cce341b648d5c765f9ac47bbd1226
Nerbyk <danil.maximov@flatstack.com>
2023-02-12 23:10:02 +0100

    setup tests

---
+++ src/client/test/functions/commands_handler_function_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+describe CommandHandlerFunction do
+  let(:described_class) { CommandHandlerFunction }
+  let(:request) do 
+    JSON.parse(
+      File.read("test/fixtures/telegram_bot_updates/command.json"),
+      symbolize_names: true
+    )
+  end 
+
+  it "must handle /start command" do
+    VCR.use_cassette("telegram_bot_api") do
+      response = described_class.process(:start, request)
+      response_body = JSON.parse(response[:body])
+
+      binding.irb
+
+      assert_equal 200, response[:statusCode] 
+      assert_equal "Hello, #{request[:message][:from][:first_name]}!", response_body["result"]["text"]
+    end
+  end
+end
--- src/client/test/test_helper.rb
+++ src/client/test/test_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-require "bundler/setup"
 
+require "bundler/setup"
 require "minitest/autorun"
 require "minitest/reporters"
 
+Dir["./test/support/**/*.rb"].each { |file| require file }
 Dir["./src/**/*.rb"].each { |file| require file }
 
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(color: true)]
